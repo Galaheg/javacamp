f10904cb2dd17fe0fa50d5771d63a1c22663595a
Hemre <galahadem@hotmail.com>
2021-05-11 01:02:53 +0300

    github push try...

---
+++ ilk/src/ilk/Main.java
@@ -0,0 +1,25 @@
+package ilk;
+
+public class Main {
+
+	public static void main(String[] args)
+{
+		
+System.out.println("Hello java");
+double xas;
+xas=8.15;
+System.out.println(xas);
+String[] krediler = {
+		"Para ver",
+		"Paraaa" };
+
+for(String kredi : krediler)
+{
+System.out.println(kredi);}
+	}
+	
+
+}
+
+
+
